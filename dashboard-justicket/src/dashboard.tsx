7c3a905926421af9cd54d018a88294436691e029
yelhassani <younes.elhassani@outlook.fr>
2023-03-13 19:36:25 +0100

    :art: dark style

--- dashboard-justicket/src/dashboard.tsx
+++ dashboard-justicket/src/dashboard.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from "recharts";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line
+} from "recharts";
+import './dashboard.css';
+
 interface IndicatorProps {
   value: number;
 }
@@ -10,7 +20,7 @@ interface GraphData {
 }
 
 interface GraphProps {
-  data: { x: string | number, y: number }[];
+  data: { x: string | number; y: number }[];
 }
 
 interface IndicatorData {
@@ -27,11 +37,16 @@ const Graph: React.FC<GraphProps> = ({ data }) => {
           <h2 className="card-title mb-0">Graph</h2>
         </div>
         <div className="card-body p-0">
-          <LineChart width={1000} height={500} data={data} margin={{ top: 20, right: 30, left: 20, bottom: 10 }}>
+          <LineChart
+            width={1000}
+            height={500}
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 10 }}
+          >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="x" />
-            <YAxis />
-            <Tooltip />
+            <XAxis dataKey="x" stroke="#ccc" />
+            <YAxis stroke="#ccc" />
+            <Tooltip contentStyle={{ backgroundColor: "#333" }} />
             <Legend />
             <Line type="monotone" dataKey="y" stroke="#8884d8" />
           </LineChart>
@@ -41,12 +56,14 @@ const Graph: React.FC<GraphProps> = ({ data }) => {
   );
 };
 
-const Indicator: React.FC<IndicatorProps & {
-  label: string;
-  onClick: () => void;
-}> = ({ value, label, onClick }) => {
+const Indicator: React.FC<
+  IndicatorProps & {
+    label: string;
+    onClick: () => void;
+  }
+> = ({ value, label, onClick }) => {
   const style = {
-    backgroundColor: "#333",
+    backgroundColor: "#222",
     color: "white",
     display: "flex",
     alignItems: "center",
@@ -56,21 +73,29 @@ const Indicator: React.FC<IndicatorProps & {
     borderRadius: "4px",
     cursor: "pointer",
     marginBottom: "10px",
+    boxShadow: "0 0 5px #333"
   };
 
   return (
     <div style={style} onClick={onClick}>
-      {label}
+      <div className="indicator-label">{label}</div>
+      <div className="indicator-value">{value}</div>
     </div>
   );
 };
 
-
-
-
-
 const Dashboard: React.FC = () => {
-  const [indicators, setIndicators] = useState<IndicatorData[]>([    {      label: "Indicator 1",      value: 60,      data: [        { x: 0, y: 10 },        { x: 1, y: 20 },        { x: 2, y: 30 },        { x: 3, y: 40 },        { x: 4, y: 50 }      ]
+  const [indicators, setIndicators] = useState<IndicatorData[]>([
+    {
+      label: "Indicator 1",
+      value: 60,
+      data: [
+        { x: 0, y: 10 },
+        { x: 1, y: 20 },
+        { x: 2, y: 30 },
+        { x: 3, y: 40 },
+        { x: 4, y: 50 }
+      ]
     },
     {
       label: "Indicator 2",
@@ -84,18 +109,16 @@ const Dashboard: React.FC = () => {
       ]
     }
   ]);
-  const [selectedIndicator, setSelectedIndicator] = useState<
-    IndicatorData | null
-  >(null);
+  const [selectedIndicator, setSelectedIndicator] = useState<IndicatorData | null>(null);
 
   const handleIndicatorClick = (indicator: IndicatorData) => {
     setSelectedIndicator(indicator);
   };
 
   return (
-    <div style={{ fontFamily: "sans-serif", padding: "20px", display: "flex" }}>
-      <div style={{ display: "flex", marginBottom: "20px" }}>
-        <div style={{ width: "70%", marginRight: "20px" }}>
+    <div style={{ fontFamily: "sans-serif", padding: "20px", backgroundColor: "#1E1E1E", color: "#fff" }}>
+      <div style={{ display: "flex", width: "100%", marginBottom: "20px" }}>
+        <div style={{ width: "30%", marginRight: "20px" }}>
           <h3 style={{ marginBottom: "10px" }}>Justicket</h3>
           {indicators.map((indicator) => (
             <Indicator
@@ -106,7 +129,7 @@ const Dashboard: React.FC = () => {
             />
           ))}
         </div>
-        <div style={{ width: "80%" }}>
+        <div style={{ width: "70%" }}>
           <h1 style={{ marginBottom: "30px" }}>Admin Dashboard</h1>
           {selectedIndicator && <Graph data={selectedIndicator.data} />}
         </div>
@@ -114,6 +137,4 @@ const Dashboard: React.FC = () => {
     </div>
   );
 };
-
-
 export default Dashboard;
